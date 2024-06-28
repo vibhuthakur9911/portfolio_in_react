f11732c3d8e66548ffa92a3d098634721b051cf1
Vaibhav <varunrajput12o8@gmail.com>
2024-06-28 19:00:41 +0530

    Create About us page, work is under processing

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,31 @@
+// import MainContent from "../utils/MainContent";
+import { useState, useEffect } from "react";
+import { TypeAnimation } from "react-type-animation";
+import abt_img from "../assets/abt_img.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDownload } from "@fortawesome/free-solid-svg-icons";
+
 const About = () => {
-  return <h1>Blog Articles</h1>;
+  return (
+    <>
+      <div className="main_content">
+        <div className="about_sec">
+          <div className="row">
+            <div className="col-md-6 col-sm-12 col-xs-12">
+              <div className="about_img">
+                {/* <img src={banner_img} alt="banner_img" className="img-fluid" /> */}
+              </div>
+            </div>
+            <div className="col-md-6 col-sm-12 col-xs-12">
+              <div className="about_img">
+                <img src={abt_img} alt="banner_img" className="img-fluid" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default About;
