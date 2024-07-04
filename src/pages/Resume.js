fcff0ac238eb45d2e83ea7cb792ec48abef5c1f6
Vaibhav <varunrajput12o8@gmail.com>
2024-07-04 18:43:51 +0530

    Updated package-lock.json, added new CSS styles, modified Resume.js to import ProgressBar and updated component structure.

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,4 +1,5 @@
 // import MainContent from "../utils/MainContent";
+import ProgressBar from "react-bootstrap/ProgressBar";
 import abt_img from "../assets/abt_img.jpg";
 import web_design from "../assets/web-design.png";
 import web_dev from "../assets/front-end-programming.png";
@@ -6,44 +7,46 @@ import log_design from "../assets/logo-design.png";
 import graphic_design from "../assets/graphic-design.png";
 import Tilt from "react-parallax-tilt";
 
-const specialize = [
+const design_skill = [
   {
     title: "Web Design",
-    desc: "Web designer specializing in front-end development. Creating visually appealing and user-friendly websites using HTML, CSS, and JavaScript to deliver engaging online experiences.",
-    img: web_design,
-  },
-  {
-    title: "Web Development",
-    desc: "Experienced front-end developer proficient in HTML, CSS, and JavaScript. Crafting responsive and interactive websites, optimizing performance, and ensuring seamless user experiences through innovative coding techniques.",
-    img: web_dev,
-  },
-  {
-    title: "Graphic Design ",
-    desc: "Graphic designer skilled in front-end development. Combining creativity and coding expertise to design visually stunning graphics, logos, and user interfaces that enhance brand identity and captivate audiences.",
-    img: graphic_design,
+    now: 50,
   },
   {
     title: "Logo Design",
-    desc: "Logo designer and front-end developer crafting impactful brand identities. Creating unique and memorable logos that effectively communicate a brand's essence, leaving a lasting impression on audiences.",
-    img: log_design,
+    now: 75,
+  },
+  {
+    title: "Graphic Design",
+    now: 75,
+  },
+];
+const database_skill = [
+  {
+    title: "MySql",
+    now: 25,
+  },
+  {
+    title: "Logo Design",
+    now: 75,
+  },
+  {
+    title: "Graphic Design",
+    now: 75,
   },
 ];
 
-function Specialized(props) {
+function MyProgress(props) {
   return (
     <>
-      <div className="col-md-3 col-sm-6 col-xs-12" key={props.j}>
-        <div className="specialize_block">
-          <div className="specialize_block_inner">
-            <img src={props.src} />
-            <h6>{props.title}</h6>
-            <p>{props.desc}</p>
-          </div>
-        </div>
+      <div className="progress_block" key =  {props.j}>
+      <ProgressBar animated now={props.now} className="myProgress" />
+      <span>{props.now}</span>
       </div>
     </>
   );
 }
+
 const Resume = (props) => {
   return (
     <>
@@ -57,6 +60,8 @@ const Resume = (props) => {
                   <span className="tag_block">2+ Years of Experienced</span>
                 </div>
               </div>
+              {/* {design_skill.map((m,j)=> <MyProgress now = {m.now} key={j} />)}
+              {database_skill.map((m,j)=> <MyProgress now = {m.now} key={j} />)} */}
             </div>
           </div>
         </div>
