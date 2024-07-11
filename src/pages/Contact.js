6c8cc9062bca48ca2f131af5e0e72fc257a61ea7
Vaibhav <varunrajput12o8@gmail.com>
2024-07-11 18:48:32 +0530

    "Added contact form styling and functionality to Style.css and Contact.js files"

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,99 @@
-const Contact = () => {
-  return <h1>Contact Me</h1>;
+const Contact = (props) => {
+  return (
+    <>
+      <div className="main_content">
+        <div className="contact_sec">
+          <div className="page_heading">
+            <h5 className="stroke_text">Contact us</h5>
+            <span className="tag_block">
+              We will happy to connect with you! 💼
+            </span>
+          </div>
+          <div className="contact">
+            <div className="contianer">
+              <div className="overLay-circle overLay-circle_1"></div>
+              <div className="overLay-circle overLay-circle_2"></div>
+              <div className="row justify-content-center">
+                <div className="col-md-6">
+                  <div class="form-block">
+                    <form
+                      id="contact-form"
+                      method="POST"
+                      // action="https://api.web3forms.com/submit"
+                    >
+                      <div id="key-access">
+                        <input
+                          type="hidden"
+                          name="access_key"
+                          value="bcb69b45-7647-403f-a791-14f0261bb02f"
+                        />
+                      </div>
+
+                      <div class="row">
+                        <div class="col-md-12">
+                          <div class="form-input">
+                            <input
+                              type="text"
+                              placeholder="Name*"
+                              id="name"
+                              name=""
+                              required=""
+                            />
+                          </div>
+                        </div>
+                        <div class="col-md-12">
+                          <div class="form-input">
+                            <input
+                              type="email"
+                              placeholder="email*"
+                              id="email"
+                              name="email"
+                              required=""
+                            />
+                          </div>
+                        </div>
+                        <div class="col-md-12">
+                          <div class="form-input">
+                            <input
+                              type="text"
+                              placeholder="Phone"
+                              id="phone"
+                              name="phone"
+                              required=""
+                              title="Please enter exactly 10 digits"
+                              pattern="[1-9]{1}[0-9]{9}"
+                              maxlength="10"
+                            />
+                          </div>
+                        </div>
+                        <div class="col-md-12">
+                          <div class="form-input">
+                            <textarea
+                              name="message"
+                              id="message"
+                              placeholder="Message..."
+                            ></textarea>
+                          </div>
+                        </div>
+
+                        <div class="col-md-12">
+                          <div class="form-input">
+                            <button type="submit" class="default-btn">
+                             <span> Send Message</span>
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Contact;
